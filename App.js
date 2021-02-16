ae04358063dc60a2b63dfb9648df5bd8bd3682af
Aditya Pratap <adityapratap7344@gmail.com>
2021-02-17 00:10:15 +0530

    added navigation

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { View, Text } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
     useFonts as useOswaldFonts,
     Oswald_400Regular,
@@ -9,8 +12,57 @@ import {
     useFonts as useLatoFonts,
     Lato_400Regular,
 } from "@expo-google-fonts/lato";
+import { Ionicons } from "@expo/vector-icons";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+    Restaurants: {
+        normal: "ios-restaurant-outline",
+        focused: "ios-restaurant",
+    },
+    Map: {
+        normal: "ios-map-outline",
+        focused: "ios-map",
+    },
+    Settings: {
+        normal: "ios-settings-outline",
+        focused: "ios-settings",
+    },
+};
+
+const SettingsScreen = () => {
+    return (
+        <SafeArea>
+            <View>
+                <Text>Settings</Text>
+            </View>
+        </SafeArea>
+    );
+};
+
+const MapScreen = () => {
+    return (
+        <SafeArea>
+            <View>
+                <Text>Map</Text>
+            </View>
+        </SafeArea>
+    );
+};
+
+const screenOptions = ({ route }) => {
+    const icon = TAB_ICON[route.name];
+    return {
+        tabBarIcon: ({ focused, color, size }) => {
+            const iconName = focused ? icon.focused : icon.normal;
+            return <Ionicons name={iconName} size={size} color={color} />;
+        },
+    };
+};
 
 export default function App() {
     const [oswaldLoaded] = useOswaldFonts({
@@ -28,7 +80,36 @@ export default function App() {
     return (
         <React.Fragment>
             <ThemeProvider theme={theme}>
-                <RestaurantScreen />
+                <NavigationContainer>
+                    <Tab.Navigator
+                        screenOptions={screenOptions}
+                        tabBarOptions={{
+                            activeTintColor: "tomato",
+                            inactiveTintColor: "gray",
+                            style: {
+                                height: "8%",
+                                minHeight: 60,
+                                maxHeight: 80,
+                                paddingVertical: 6,
+                            },
+                            labelStyle: {
+                                fontWeight: "500",
+                                fontSize: 12.75,
+                                marginBottom: 4,
+                            },
+                        }}
+                    >
+                        <Tab.Screen
+                            name="Restaurants"
+                            component={RestaurantScreen}
+                        />
+                        <Tab.Screen name="Map" component={MapScreen} />
+                        <Tab.Screen
+                            name="Settings"
+                            component={SettingsScreen}
+                        />
+                    </Tab.Navigator>
+                </NavigationContainer>
             </ThemeProvider>
             <ExpoStatusBar style="auto" />
         </React.Fragment>
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const RestaurantScreen = () => {
     return (
@@ -10,23 +12,36 @@ export const RestaurantScreen = () => {
             <SearchContainer>
                 <Searchbar placeholder="Search" />
             </SearchContainer>
-            <RestaurantListContainer>
-                <RestaurantInfoCard />
-            </RestaurantListContainer>
+            <RestaurantList
+                data={[
+                    { name: "Rest 1" },
+                    { name: "Rest 2" },
+                    { name: "Rest 3" },
+                    { name: "Rest 4" },
+                    { name: "Rest 5" },
+                    { name: "Rest 6" },
+                    { name: "Rest 7" },
+                    { name: "Rest 8" },
+                    { name: "Rest 9" },
+                    { name: "Rest 10" },
+                ]}
+                renderItem={() => (
+                    <Spacer position="bottom" size="large">
+                        <RestaurantInfoCard />
+                    </Spacer>
+                )}
+                keyExtractor={(item) => item.name}
+            />
         </SafeArea>
     );
 };
-
-const SafeArea = styled(SafeAreaView)`
-    flex: 1;
-    margin-top: ${StatusBar.currentHeight}px;
-`;
 
 const SearchContainer = styled.View`
     padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-    flex: 1;
-    padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+    contentContainerStyle: {
+        padding: 16,
+    },
+})``;
